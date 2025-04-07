08415d90b0903b9b5e095220ce34516a0c726037
CaiJi_Feizao <1077386304@qq.com>
2025-04-07 19:27:55 +0800

    INF NAN

--- lab1_stu/fp_add.asm
+++ lab1_stu/fp_add.asm
@@ -1,5 +1,5 @@
 .data
-    float1: .float -9.777
+    float1: .float -9.232
     float2: .float -1.234
 
 .macro push %a
@@ -58,26 +58,47 @@ MYADD:
     and s7, a3, t2
     or s6, s6, t1
     or s7, s7, t1
-
+	# checknan inf
+	lui t1, 0
+	addi t1, t1, 0xff
+	beq s4, t1, NAN_INF_A
+	beq s5, t1, NAN_INF_B
+	beq t1, t1, NORMAL
+NAN_INF_A:
+	beq zero, s6, NAN
+	beq zero, zero, INF
+NAN_INF_B:	
+    beq zero, s7, NAN
+    beq zero, zero, INF
+NAN:
+	lui, a0, 0x00000
+	beq zero, zero, RETURN
+IF_INF:
+	beq s2, s3, INF
+	beq zero, zero, NAN
+INF:
+	srli s2, s2, 31
+	lui a0, 0x7f800
+	and a0, a0, s2
+	beq zero, zero, RETURN
+NORMAL:
     sub s8, s4, s5
     srl s7, s7, s8
-
     add s8, s6, s7
 	
 	lui t1, 0xff000
     and s9, s8, t1
     
-    beq s9, zero, NOTRIGHT
+    beq s9, zero, ALIGNSKIP
     srli s8, s8, 1
     addi s4, s4, 1
-NOTRIGHT:
-
+ALIGNSKIP:
     and s8, s8, t2
     slli s2, s2, 31
     slli s4, s4, 23
     or a0, s2, s4
     or a0, a0, s8
-
+RETURN:
     pop s8
     pop s7
     pop s6
